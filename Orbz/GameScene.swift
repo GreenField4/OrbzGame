7c1bae724e5ab51181f3e28e7a5f53a180b91c39
Andrew Greenfield <gree6310@n2095-mac-07.local>
2018-03-22 18:38:43 -0400

    Initial commit

--- Orbz/GameScene.swift
+++ Orbz/GameScene.swift
@@ -11,62 +11,55 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+    let btnToggleMute = SKLabelNode(fontNamed: "Courier")
+    let btnPlay = SKLabelNode(fontNamed: "Courier")
+    let btnControls = SKLabelNode(fontNamed: "Courier")
     override func didMove(to view: SKView) {
+        backgroundColor = SKColor.black
+        //play button
+        print("Play button created")
+        btnPlay.fontColor = SKColor.black
+        btnPlay.fontSize = 60
+        btnPlay.text = "PLAY"
+        btnPlay.name = "btnPlay"
+        btnPlay.position =  CGPoint(x:self.frame.midX, y:self.frame.midY+200);
+        self.addChild(btnPlay)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        //instruction button
+        print("Instruction button created")
+        btnControls.fontColor = SKColor.black
+        btnControls.fontSize = 50
+        btnControls.text = "Controls"
+        btnControls.name = "btnControls"
+        btnControls.position =  CGPoint(x:self.frame.midX, y:self.frame.midY+100);
+        self.addChild(btnControls)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        print("Play Music button created")
+        btnToggleMute.fontColor = SKColor.black
+        btnToggleMute.fontSize = 40
+        btnToggleMute.text = "Play Music"
+        btnToggleMute.name = "btnToggleMute"
+        btnToggleMute.position =  CGPoint(x:self.frame.midX, y:self.frame.midY);
+        self.addChild(btnToggleMute)
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
---
+++ Orbz/GameVariables.swift
@@ -0,0 +1,14 @@
+//
+//  GameVariables.swift
+//  Orbz
+//
+//  Created by Andrew Greenfield on 2018-03-22.
+//  Copyright © 2018 Andrew Greenfield. All rights reserved.
+//
+
+import Foundation
+struct GameVariables {
+    var toggleMute = true
+    var curScore = 0
+    
+}
